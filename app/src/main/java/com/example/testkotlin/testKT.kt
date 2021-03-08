1a8ee99e3c12a4a7129c613d5ec71d6a270ad7e6
lly-rachel <970758884@qq.com>
2021-03-08 22:02:50 +0800

    vararg：函数边长参数的使用 Toast和Snackbar的简化用法

--- app/src/main/java/com/example/testkotlin/testKT.kt
+++ app/src/main/java/com/example/testkotlin/testKT.kt
@@ -1,5 +1,11 @@
 package com.example.testkotlin
 
+import android.content.Context
+import android.view.View
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import java.lang.RuntimeException
+import java.time.Duration
 import kotlin.math.max
 
 
@@ -8,10 +14,10 @@ fun main(){
 
     val a = 10
     println("\na = "+a)
-    println("-->"+ Large(45,90))
+    println("-->${Large(45,90)}")
 
-
-
+    println("------max------${maxNum(12,58,96,56,1,0,77,11,-8)}")
+    println("------max------${maxNumber(12.58,96.56,1.0,77.11,-8.0)}")
 
     /***************************************************/
     //for in 循环
@@ -48,8 +54,58 @@ fun Large(para2: Int,para1: Int):Int{
     return max(para1,para2)
 }
 
+fun maxNum(vararg nums:Int):Int{
+    var maxNum = Int.MIN_VALUE
+    for(num in nums){
+        maxNum = max(maxNum,num)
+    }
+    return maxNum
+}
+
+fun <T:Comparable<T>> maxNumber(vararg nums:T):T{
+    if(nums.isEmpty()) throw RuntimeException("Params can't be empty.")
+    var maxNum = nums[0]
+    for(num in nums){
+        if(maxNum<num){
+            maxNum = num
+        }
+    }
+    return maxNum
+}
+
+
 fun getScore(name: String) = when(name){
     "Tom" -> 86
     "Jim" -> 77
     else -> 0
+}
+
+fun String.showToast(context: Context,duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(context,this,duration).show()
+}
+
+fun Int.showToast(context: Context,duration: Int = Toast.LENGTH_SHORT){
+    Toast.makeText(context,this,duration).show()
+}
+
+fun View.showSnackbar(text :String,actionText:String?=null,
+    duration: Int =  Snackbar.LENGTH_SHORT,block:(()->Unit)? = null){
+    val snackbar = Snackbar.make(this,text, duration)
+    if(actionText!=null&&block!=null){
+        snackbar.setAction(actionText){
+            block()
+        }
+    }
+    snackbar.show()
+}
+
+fun View.showSnackbar(resId:Int,actionResId:Int ?=null,
+                      duration: Int =  Snackbar.LENGTH_SHORT,block:(()->Unit)? = null){
+    val snackbar = Snackbar.make(this,resId, duration)
+    if(actionResId!=null&&block!=null){
+        snackbar.setAction(actionResId){
+            block()
+        }
+    }
+    snackbar.show()
 }
